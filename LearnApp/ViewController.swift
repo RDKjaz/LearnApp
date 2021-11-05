ab2b908812c95cf8c95f5aa6355c0880ae55f41d
Radik <RDKJaz@gmail.com>
2021-11-06 00:23:37 +0300

    Initial Commit

---
+++ LearnApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnApp
+//
+//  Created by Radik Gazetdinov on 06.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
